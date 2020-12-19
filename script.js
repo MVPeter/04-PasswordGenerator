4ad85154e6bb2a50598a85a6888dc5744b702f60
MattV <mvpetersen1@gmail.com>
2020-12-19 13:36:14 -0700

    updated error handling in JS.

--- script.js
+++ script.js
@@ -3,108 +3,111 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-  
+    passwordText.value = password;
+
 }
 
 function generatePassword() {
-  // Prompt for password length.  Continue when input is in range
-  var prompPWLenght = prompt("Please choose a password lenght between 8 and 128")
-        while (prompPWLenght < 8 || prompPWLenght > 128) {
-            var prompPWLenght = prompt("Please choose a password lenght between 8 and 128");
-        }
+    // Prompt for password length.  Continue when input is in range
+    var prompPWLenght = prompt("Please choose a password lenght between 8 and 128")
+    while (prompPWLenght < 8 || prompPWLenght > 128) {
+        var prompPWLenght = prompt("Please choose a password lenght between 8 and 128");
+    }
 
-        //Prompt for input of character pool.
-        var acsiiCharaters = ["!#$%&'()*+,-./:;<=>?@[]^_`{|}", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", "1234567890"];
-        var pSpecial = confirm("Include Special characters?:   " + acsiiCharaters[0]);
-        var pCapital = confirm("Include Capital Letters?:   " + acsiiCharaters[1]);
-        var pLowercase = confirm("Include Lower Case Letters?:    " + acsiiCharaters[2]);
-        var pNumbers = confirm("Include Numbers:    " + acsiiCharaters[3]);
-        
-        //check which options were chosen. 
-        if (pSpecial === true) {
-            var tSpecial = [acsiiCharaters[0]]
-        } else {
-            var tSpecial = "";
-        }
+    //Prompt for input of character pool.
+    var acsiiCharaters = ["!#$%&'()*+,-./:;<=>?@[]^_`{|}", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", "1234567890"];
+    do {
+    alert("Please choose at least one of the following: ")
+    var pSpecial = confirm("Include Special characters?:   " + acsiiCharaters[0]);
+    var pCapital = confirm("Include Capital Letters?:   " + acsiiCharaters[1]);
+    var pLowercase = confirm("Include Lower Case Letters?:    " + acsiiCharaters[2]);
+    var pNumbers = confirm("Include Numbers:    " + acsiiCharaters[3]);
+    } while (pSpecial === false && pCapital === false && pLowercase === false && pNumbers === false) 
 
-        if (pCapital === true) {
-            var tCapital = [acsiiCharaters[1]]
-        } else {
-            var tCapital = "";
-        }
+    //check which options were chosen. 
+    if (pSpecial === true) {
+        var tSpecial = [acsiiCharaters[0]];
+    } else {
+        var tSpecial = "";
+    }
 
-        if (pLowercase === true) {
-            var tLowercase = [acsiiCharaters[2]]
-        } else {
-            var tLowercase = "";
-        }
+    if (pCapital === true) {
+        var tCapital = [acsiiCharaters[1]];
+    } else {
+        var tCapital = "";
+    }
 
-        if (pNumbers === true) {
-            var tNumbers = [acsiiCharaters[3]]
-        } else {
-            var tNumbers = "";
-        }
-       
-        var asciiAllChars = (tSpecial + tCapital + tLowercase + tNumbers);
+    if (pLowercase === true) {
+        var tLowercase = [acsiiCharaters[2]];
+    } else {
+        var tLowercase = "";
+    }
 
-        var allCharsArray = asciiAllChars.split("");
+    if (pNumbers === true) {
+        var tNumbers = [acsiiCharaters[3]];
+    } else {
+        var tNumbers = "";
+    }
 
-        
-        //Check if there were any character set choices made    
-        if (asciiAllChars === "") {
-            alert("You have not selected any characters please click Generate again.")
-            throw new Error("You have not selected any characters please click Generate again.");
-        } else {
-            alert("Your password will include the following characters:  " + "\r" + "\r" + tSpecial + "\r" + tCapital + "\r " + tLowercase + "\r " + tNumbers);
-        }
-        var PWLength = asciiAllChars.length;
+    var asciiAllChars = (tSpecial + tCapital + tLowercase + tNumbers);
 
-        //Random number array, chosen length
-        
-        var RNumbers = [];
-        for (var i = 0; i < prompPWLenght; i++) {
-            RNumbers.push(Math.floor(Math.random() * PWLength))
-        }
+    var allCharsArray = asciiAllChars.split("");
 
-        //Use the Randon Numbers array to Select Characters from asciiAllchars array
-        var PWString = "";
-        for (var i = 0; i < prompPWLenght; i++) {
-            PWString += asciiAllChars[RNumbers[i]];
-        }
-        return PWString
+
+    //Check if there were any character set choices made    
+    if (asciiAllChars === "") {
+        alert("You have not selected any characters please click Generate again.")
+        throw new Error("You have not selected any characters please click Generate again.");
+    } else {
+        alert("Your password will include the following characters:  " + "\r" + "\r" + tSpecial + "\r" + tCapital + "\r " + tLowercase + "\r " + tNumbers);
+    }
+    var PWLength = asciiAllChars.length;
+
+    //Random number array, chosen length
+    var RNumbers = [];
+    for (var i = 0; i < prompPWLenght; i++) {
+        RNumbers.push(Math.floor(Math.random() * PWLength));
+    }
+
+    //Use the Randon Numbers array to Select Characters from asciiAllchars array
+    var PWString = "";
+    for (var i = 0; i < prompPWLenght; i++) {
+        // PWString += asciiAllChars[RNumbers[i]];
+        PWString += allCharsArray[RNumbers[i]];
+    }
+
+
+    // var asciiUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    console.log(tCapital.length);
+    console.log(tCapital);
+    // var asciiLowerCase = asciiUpperCase.toLowerCase();
+    console.log(tLowercase.length);
+    console.log(tLowercase);
+    // var numbDigits = "1234567890";
+    console.log(tNumbers.length);
+    console.log(tNumbers);
+    // var asciiSpecial = "!#$%&'()*+,-./:;<=>?@[]^_`{|}";
+    console.log(tSpecial.length);
+    console.log(tSpecial);
+    // var asciiAllChars = asciiUpperCase + asciiLowerCase + numbDigits + asciiSpecial;
+    console.log(asciiAllChars);
+    console.log(allCharsArray.length);
+    console.log(allCharsArray);
+    //asciiAllChars.length.  Used to determine random number Max
+    console.log(asciiAllChars.length);
+    // RNumbers.  Array with the randon number 0 - user choice
+    console.log(RNumbers);
+    // PWArray. this is ascii pool created from the randon numbers
+    console.log(PWString.length);
+    console.log(prompPWLenght);
+    console.log(PWString);
+
+    return PWString;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-        // var asciiUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        console.log(tCapital.length);
-        console.log(tCapital);
-        // var asciiLowerCase = asciiUpperCase.toLowerCase();
-        console.log(tLowercase.length);
-        console.log(tLowercase);
-        // var numbDigits = "1234567890";
-        console.log(tNumbers.length);
-        console.log(tNumbers)
-        // var asciiSpecial = "!#$%&'()*+,-./:;<=>?@[]^_`{|}";
-        console.log(tSpecial.length);
-        console.log(tSpecial)
-        // var asciiAllChars = asciiUpperCase + asciiLowerCase + numbDigits + asciiSpecial;
-        console.log(asciiAllChars)
-        console.log(allCharsArray.length)
-        console.log(allCharsArray)
-
-        //asciiAllChars.length.  Used to determine random number Max
-        console.log(asciiAllChars.length);
-
-        // RNumbers.  Array with the randon number 0 - user choice
-        console.log(RNumbers);
-
-        // PWArray. this is ascii pool created from the randon numbers
-        console.log(PWString.length)
-        console.log(prompPWLenght)
-        console.log(PWString)        
